86bcece98f02a67f50ee2a24cce916b370e2df2f
keeper <keeper402@outlook.com>
2024-06-16 22:03:02 +0800

    [done]完成零知识证明红包领取方案

--- contracts/Hongbao.sol
+++ contracts/Hongbao.sol
@@ -1,47 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
 
-contract Hongbao {
+import "hardhat/console.sol";
+import "./verifier.sol";
+
+contract Hongbao is Groth16Verifier {
 
     enum ReceiveType {
         AVG,
         RAND
+    }
+    // 证明结构体
+    struct Proof {
+        uint256[2] a;
+        uint256[2][2] b;
+        uint256[2] c;
     }
 
     // 红包结构体
     struct HongbaoInfo {
         address sender;// 发送人
         uint256 passwordHash; //snarkjs计算的hash
-        mapping(address => bool) openedAddr; // 记录已经领取的红包的地址
+        mapping(address => bool) claimedAddr; // 记录已经领取的红包的地址
         uint total; // 总份数
         uint remaining; // 剩余份数
         uint256 totalAmount; // 红包总金额
         uint256 remainingAmount; // 剩余金额
+        ReceiveType receiveType;//领取类型
     }
 
     //口令 -> 红包
     mapping(uint256 => HongbaoInfo) internal hongbaoMap;
 
+    //口令 -> 红包
+    mapping(uint256 => HongbaoInfo[]) internal fullyClaimedHongbaoMap;
+
     // 创建红包事件
-    event HongbaoCreated(bytes32 indexed keyHash,  uint total, uint amount, ReceiveType receiveType, address indexed creator);
+    event HongbaoCreated(bytes32 indexed passwordHash, uint total, uint amount, ReceiveType receiveType, address indexed creator);
 
     // 领取红包事件
-    event RedPacketClaimed(bytes32 indexed keyHash, bytes32 doubleHashedKey, address indexed claimer, uint256 amount);
+    event HongbaoClaimed(bytes32 indexed passwordHash, address indexed claimer, uint256 claimAmount, bytes32 remaining, bytes32 remainingAmount);
 
     // 创建红包
     function createHongbao(uint256 passwordHash, uint total, uint amount, ReceiveType receiveType) public payable {
         //amount设计考虑未来引入其他链
         require(msg.value == amount, "Invalid value");
         require(msg.value > 0, "Red packet amount must be greater than 0");
-
+        require(total > 0, "total must be greater than 0");
         HongbaoInfo storage h = hongbaoMap[passwordHash];
+        require(h.remaining == 0, "Please use another password");
+        //重复的时候，进行红包归档
+        if (h.total > 0) {
+            // old packet store
+            HongbaoInfo memory temp = h;  // 保存要转移的结构体到临时变量
+            delete hongbaoMap[passwordHash]; // 从原映射中删除元素并重置为默认值
+            fullyClaimedHongbaoMap[passwordHash].push(temp);  // 将保存的结构体添加到另一个映射中
+        }
+
         h.sender = msg.sender;
         h.passwordHash = passwordHash;
         h.total = total;
         h.remaining = total;
         h.totalAmount = msg.value;
         h.remainingAmount = msg.value;
+        h.receiveType = receiveType;
         console.log("packet created, totalAmount %s, packet number %s", h.totalAmount, total);
 
         emit HongbaoCreated(passwordHash, total, amount, msg.sender, receiveType);
@@ -53,7 +75,40 @@ contract Hongbao {
     }
 
     // 领取红包
-    function claimHongbao(uint256 passwordHash, Proof proof) public {
-        //todo 零知识证明 领取红包
+    function claimHongbao(uint256 passwordHash, Proof calldata proof) public {
+        HongbaoInfo storage hongbao = hongbaoMap[passwordHash];
+        require(hongbao.remaining > 0, "hongbao fully claimed");
+        require(!hongbao.claimedAddr[msg.sender], "you already claimed hongbao");
+
+        //零知识证明 验证领取者知道口令
+        bool verify = verifyProof(proof.a, proof.b, proof.c, [passwordHash, uint256(uint160(msg.sender))]);
+        require(verify, "zero knowledge verify fail");
+
+        uint256 claimAmount;
+        if (hongbao.remaining <= 1) {
+            claimAmount = hongbao.remainingAmount;
+        } else {
+            claimAmount = getClaimAmount(hongbao);
+        }
+        // 领取红包
+        payable(msg.sender).transfer(claimAmount);
+        hongbao.remaining--;
+        hongbao.remainingAmount-= claimAmount;
+        hongbao.claimedAddr[msg.sender] = true;
+        emit HongbaoClaimed(passwordHash, msg.sender, claimAmount, hongbao.remaining, hongbao.remainingAmount);
+
+    }
+
+    function getClaimAmount(HongbaoInfo hongbao) private returns (uint256){
+        if (hongbao.receiveType == ReceiveType.AVG) {
+            return hongbao.totalAmount / hongbao.total;
+        }
+        //取一个范围在 1~hongbao.remainingAmount（包含） 之间的随机数
+        if (hongbao.receiveType == ReceiveType.RAND) {
+            uint256 randomFactor = uint256(keccak256(abi.encodePacked(block.timestamp, hongbao.remainingAmount, msg.sender)))
+                % hongbao.remainingAmount;
+            return randomFactor + 1;
+        }
+        revert("invalid hongbao receiveType");
     }
 }
---
+++ contracts/verifier.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+    Copyright 2021 0KIMS association.
+
+    This file is generated with [snarkJS](https://github.com/iden3/snarkjs).
+
+    snarkJS is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snarkJS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Groth16Verifier {
+    // Scalar field size
+    uint256 constant r    = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    // Base field size
+    uint256 constant q   = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+    // Verification Key data
+    uint256 constant alphax  = 1936270969976247635049423608918025248732878627754266091693729236969426140836;
+    uint256 constant alphay  = 17424880929000187812493394113383379568190370324392372130607824133160885862226;
+    uint256 constant betax1  = 9690109209818846674535504716833662932803892733113101548307866016686438558424;
+    uint256 constant betax2  = 12288805594839934252822700234251762985254295768709908063746304027703684734897;
+    uint256 constant betay1  = 39116800995306654438449845741192281407643174399662593693631822418732964193;
+    uint256 constant betay2  = 2007151962509885362331617024280584245108540778151269443161666708197762110195;
+    uint256 constant gammax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 constant gammax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 constant gammay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 constant gammay2 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;
+    uint256 constant deltax1 = 6613353245013058766023488028469206713464511262765933785603900407741220938102;
+    uint256 constant deltax2 = 20177836076099912789636866478157245011891128157279860910703599144501854720992;
+    uint256 constant deltay1 = 15232165599047633898320305092511310784058233320798165319488118350588300172632;
+    uint256 constant deltay2 = 7512029621440787161396334794794698216579850225861452372280848628432200624942;
+
+    
+    uint256 constant IC0x = 9699760157469666083358941920288208050716902517899496647836409398995822979327;
+    uint256 constant IC0y = 11973292224521023024867026518227126457519313563982598980609897488007089513661;
+    
+    uint256 constant IC1x = 14724486754664837161527941627854935927614419615545215792010100071813165586928;
+    uint256 constant IC1y = 7811433088745411154236751339890784322304893224059871723430639178221207103093;
+    
+    uint256 constant IC2x = 16989471650566275685673906621060778232030601218592102078815556708644319421775;
+    uint256 constant IC2y = 3326372683720075002523838268046946965430738374337139409867286658691791368814;
+    
+ 
+    // Memory data
+    uint16 constant pVk = 0;
+    uint16 constant pPairing = 128;
+
+    uint16 constant pLastMem = 896;
+
+    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[2] calldata _pubSignals) public view returns (bool) {
+        assembly {
+            function checkField(v) {
+                if iszero(lt(v, r)) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+            
+            // G1 function to multiply a G1 value(x,y) to value in an address
+            function g1_mulAccC(pR, x, y, s) {
+                let success
+                let mIn := mload(0x40)
+                mstore(mIn, x)
+                mstore(add(mIn, 32), y)
+                mstore(add(mIn, 64), s)
+
+                success := staticcall(sub(gas(), 2000), 7, mIn, 96, mIn, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+
+                mstore(add(mIn, 64), mload(pR))
+                mstore(add(mIn, 96), mload(add(pR, 32)))
+
+                success := staticcall(sub(gas(), 2000), 6, mIn, 128, pR, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+
+            function checkPairing(pA, pB, pC, pubSignals, pMem) -> isOk {
+                let _pPairing := add(pMem, pPairing)
+                let _pVk := add(pMem, pVk)
+
+                mstore(_pVk, IC0x)
+                mstore(add(_pVk, 32), IC0y)
+
+                // Compute the linear combination vk_x
+                
+                g1_mulAccC(_pVk, IC1x, IC1y, calldataload(add(pubSignals, 0)))
+                
+                g1_mulAccC(_pVk, IC2x, IC2y, calldataload(add(pubSignals, 32)))
+                
+
+                // -A
+                mstore(_pPairing, calldataload(pA))
+                mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
+
+                // B
+                mstore(add(_pPairing, 64), calldataload(pB))
+                mstore(add(_pPairing, 96), calldataload(add(pB, 32)))
+                mstore(add(_pPairing, 128), calldataload(add(pB, 64)))
+                mstore(add(_pPairing, 160), calldataload(add(pB, 96)))
+
+                // alpha1
+                mstore(add(_pPairing, 192), alphax)
+                mstore(add(_pPairing, 224), alphay)
+
+                // beta2
+                mstore(add(_pPairing, 256), betax1)
+                mstore(add(_pPairing, 288), betax2)
+                mstore(add(_pPairing, 320), betay1)
+                mstore(add(_pPairing, 352), betay2)
+
+                // vk_x
+                mstore(add(_pPairing, 384), mload(add(pMem, pVk)))
+                mstore(add(_pPairing, 416), mload(add(pMem, add(pVk, 32))))
+
+
+                // gamma2
+                mstore(add(_pPairing, 448), gammax1)
+                mstore(add(_pPairing, 480), gammax2)
+                mstore(add(_pPairing, 512), gammay1)
+                mstore(add(_pPairing, 544), gammay2)
+
+                // C
+                mstore(add(_pPairing, 576), calldataload(pC))
+                mstore(add(_pPairing, 608), calldataload(add(pC, 32)))
+
+                // delta2
+                mstore(add(_pPairing, 640), deltax1)
+                mstore(add(_pPairing, 672), deltax2)
+                mstore(add(_pPairing, 704), deltay1)
+                mstore(add(_pPairing, 736), deltay2)
+
+
+                let success := staticcall(sub(gas(), 2000), 8, _pPairing, 768, _pPairing, 0x20)
+
+                isOk := and(success, mload(_pPairing))
+            }
+
+            let pMem := mload(0x40)
+            mstore(0x40, add(pMem, pLastMem))
+
+            // Validate that all evaluations ∈ F
+            
+            checkField(calldataload(add(_pubSignals, 0)))
+            
+            checkField(calldataload(add(_pubSignals, 32)))
+            
+            checkField(calldataload(add(_pubSignals, 64)))
+            
+
+            // Validate all evaluations
+            let isValid := checkPairing(_pA, _pB, _pC, _pubSignals, pMem)
+
+            mstore(0, isValid)
+             return(0, 0x20)
+         }
+     }
+ }
