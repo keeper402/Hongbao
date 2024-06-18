6f4baaee2a0d6bbfed0aac14f4d730f3e87faecb
xu8117 <xu8117@gmail.com>
2024-06-18 21:32:28 +0800

    feat: frontend commit

---
+++ frontend/hongbao-blockchain/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CreateRedPacket />
+  </div>
+</template>
+
+<script>
+import CreateRedPacket from "@/components/CreateRedPacket.vue";
+
+export default {
+  name: "App",
+  components: {
+    CreateRedPacket,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
