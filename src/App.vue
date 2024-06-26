34899b1b112b0d24a8cca5a5cd9ba106987ee3f6
sjpsega <ssjjpp520@126.com>
2024-06-26 17:22:19 +0800

    style: add bgImage

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,3 @@ export default {
   },
 };
 </script>
-
-<style>
-body {
-  background-color: black !important;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
-}
-</style>
